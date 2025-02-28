4bdebed1f8c76c0d47deb48015a3ff5e4b82cba7
Tehreem <tanitani297@gmail.com>
2025-02-28 16:14:46 +0500

    Add files via upload

---
+++ class1-assignment/main .py
@@ -0,0 +1,31 @@
+# first class
+
+print("Hello World")
+
+
+# Pycache Example
+
+def greet (name):
+    return f"Hello, {name}!"
+print(greet("Tehreem Abdullah"))
+   
+
+# data types
+
+name = "Tehreem Abdullah"                                          # string
+
+age = 19                                                    # integer
+
+height = 5.5                                                 # float
+
+is_student = True                                           # boolean
+
+grades = [95, 83, 90, 87, 74]                                # list
+
+coordinates = (3, 5)                                         # tuple
+
+person = {"name": "Tehreem Abdullah", "age": 19, "height": 5.5}      # dictionary
+                                            
+unique_numbers = {1, 2, 3, 4, 5}                               # set
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
